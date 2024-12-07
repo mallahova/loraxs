0631d707ed98e2be1ae2524c00bfb0bbc33bdf11
mallahova <alismalahova31@gmail.com>
2024-12-07 15:35:18 +0100

    directory restructuring for cola task

---
+++ my_experiments/tasks/task1/from_scratch/train_cola.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export WANDB_RUN_ID="l25vss04"
+export WANDB_RESUME=true
+export PYTHONPATH=$(pwd) # should be LoRa-XS folder
+python my_experiments/tasks/task1/from_scratch/run_glue.py --target_task cola --wandb_diasabled True
+# python my_experiments/tasks/task1/run_glue_pretrained.py --target_task mrpc --wandb_diasabled False
