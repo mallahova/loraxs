Fixed rank masking

main_glue_rank_masking.py
@@ -259,7 +259,6 @@ class ModelArguments:
     )
 
 
-
 def main():
     # See all possible arguments in src/transformers/training_args.py
     # or by passing the --help flag to this script.
@@ -691,7 +690,7 @@ def main():
         metric = evaluate.load("accuracy")
 
     # You can define your custom compute_metrics function. It takes an `EvalPrediction` object (a namedtuple with a
-    # predictions and label_ids field) and has to return a dictionary string to float
+    # predictions and label_ids field) and has to return a dictionary string to float.
     def compute_metrics(p: EvalPrediction):
         preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
         preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
@@ -699,6 +698,7 @@ def main():
         if len(result) > 1:
             result["combined_score"] = np.mean(list(result.values())).item()
         return result
+
     # Data collator will default to DataCollatorWithPadding when the tokenizer is passed to Trainer, so we change it if
     # we already did the padding.
     if data_args.pad_to_max_length:

utils/initialization_utils_rank_masking.py
@@ -29,7 +29,7 @@ class WeightMaskingLinear(torch.nn.Linear):
                 f"Mask shape {mask.shape} does not match weight shape {self.weight.shape}."
             )
         
-        self.mask = mask.to(self.weight.device)  
+        self.mask = self.mask.to(self.weight.device)  
 
     def reset_mask(self):
         self.mask = None
@@ -39,6 +39,7 @@ class WeightMaskingLinear(torch.nn.Linear):
         Apply the mask and compute the forward pass.
         """
         if self.mask is not None:
+            self.mask = self.mask.to(self.weight.device)  
             masked_weight = self.weight * self.mask
         else:
             masked_weight = self.weight