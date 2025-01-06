Fixed rank masking

utils/initialization_utils_rank_masking.py
@@ -123,8 +123,6 @@ def find_and_initialize(
     :param reconstr_type: options: 'svd'
     """
     rank_masking = reconstruct_config.get("rank_masking", False)
-    if rank_masking:
-        print("Using rank masking")
     half_init_dec = reconstruct_config["half_init_dec"]
     replacement_module_random_init = reconstruct_config[
         "replacement_module_random_init"
@@ -186,15 +184,6 @@ def find_and_initialize(
                         replace_module_weights(
                             target.lora_A.default, replacement_encoder_weight.T
                         )
-                        target.default_lora_latent_mapping = torch.nn.Linear(
-                            lora_config.r, lora_config.r, bias=False
-                        )
-                        init_module_weights(
-                            target.default_lora_latent_mapping, sigma=0.00001
-                        )
-                        target.default_lora_latent_mapping.to(
-                            target.lora_A.default.weight.device
-                        )
 
                         if rank_masking:
                             target.default_lora_latent_mapping = WeightMaskingLinear(
@@ -204,6 +193,19 @@ def find_and_initialize(
                             target.default_lora_latent_mapping = torch.nn.Linear(
                                 lora_config.r, lora_config.r, bias=False
                             )
+
+                        init_module_weights(
+                            target.default_lora_latent_mapping, sigma=0.00001
+                        )
+                        target.default_lora_latent_mapping.to(
+                            target.lora_A.default.weight.device
+                        )
+                        target.lora_A.default.weight.requires_grad = (
+                            False  # only the r*r matrix will be tuned
+                        )
+                        target.lora_B.default.weight.requires_grad = (
+                            False  # only the r*r matrix will be tuned
+                        )
 
                     else:
                         init_module_weights(target.lora_A.default, sigma=0.00001)

utils/latent_utils.py
@@ -32,10 +32,13 @@ def get_delta_weight(self, adapter) -> torch.Tensor:
         weight_A = weight_A.float()
         weight_B = weight_B.float()
 
-    output_tensor = transpose(
-        weight_B @ self.default_lora_latent_mapping.weight @ weight_A,
-        self.fan_in_fan_out
-    ) * self.scaling[adapter]
+    output_tensor = (
+        transpose(
+            weight_B @ self.default_lora_latent_mapping.weight @ weight_A,
+            self.fan_in_fan_out,
+        )
+        * self.scaling[adapter]
+    )
 
     if cast_to_fp32:
         output_tensor = output_tensor.to(dtype=dtype)
@@ -55,9 +58,13 @@ def forward_latent(self, x: torch.Tensor):
     if self.disable_adapters:
         if self.r[self.active_adapter[0]] > 0 and self.merged:
             self.unmerge()
-        result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
+        result = F.linear(
+            x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias
+        )
     elif self.r[self.active_adapter[0]] > 0 and not self.merged:
-        result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
+        result = F.linear(
+            x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias
+        )
 
         x = x.to(self.lora_A[self.active_adapter[0]].weight.dtype)
 
@@ -65,15 +72,18 @@ def forward_latent(self, x: torch.Tensor):
         result += (
             self.lora_B[self.active_adapter[0]](
                 self.default_lora_latent_mapping(
-                    self.lora_A[self.active_adapter[0]](self.lora_dropout[self.active_adapter[0]](x))
+                    self.lora_A[self.active_adapter[0]](
+                        self.lora_dropout[self.active_adapter[0]](x)
+                    )
                 )
             )
             * self.scaling[self.active_adapter[0]]
         )
     else:
-        result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
+        result = F.linear(
+            x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias
+        )
 
     result = result.to(previous_dtype)
 
     return result
-