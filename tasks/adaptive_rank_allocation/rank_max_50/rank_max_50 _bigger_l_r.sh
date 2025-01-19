9ba30a016ddeeca25acfbc28e96f768bda854742
mallahova <alismalahova31@gmail.com>
2025-01-19 15:46:54 +0100

    Added more experiments

---
+++ tasks/adaptive_rank_allocation/rank_max_50/rank_max_50 _bigger_l_r.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=adaptive_loraxs
+#SBATCH --partition=dgx
+#SBATCH --qos=normal
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=10
+
+
+for rank_allocation_learning_rate in 0.02 0.05 0.1; do
+  export WANDB_PROJECT="adaptive_rank_allocation"
+  export CUDA_VISIBLE_DEVICES=0  # Assign GPU 0
+  export WANDB_NOTES="rank_allocation_weights initialized to zero, same scheduling, rank_max 50, average rank 20, discrete rank on the last epoch, rank_min is 5" # run description
+  python scripts/run_glue_adaptive.py --target_task cola --wandb_disabled False --seed 0 --rank_allocation_learning_rate $rank_allocation_learning_rate --rank_max 50 --rank_average 20 --epoch 51 --rank_min 5 &
+done
+
+wait
