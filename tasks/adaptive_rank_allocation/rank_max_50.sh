45a5eb52c70cea96ca51d9aa1decc545006f0d53
mallahova <alismalahova31@gmail.com>
2025-01-19 13:00:48 +0100

    Fixed rank_average initialization

--- tasks/adaptive_rank_allocation/rank_max_50.sh
+++ tasks/adaptive_rank_allocation/rank_max_50.sh
@@ -9,8 +9,8 @@
 for rank_allocation_learning_rate in 0.002 0.005 0.01; do
   export WANDB_PROJECT="adaptive_rank_allocation"
   export CUDA_VISIBLE_DEVICES=0  # Assign GPU 0
-  export WANDB_NOTES="rank_allocation_weights initialized to zero, same scheduling, rank_max 50, average rank 20" # run description
-  python scripts/run_glue_adaptive.py --target_task cola --wandb_disabled False --seed 0 --rank_allocation_learning_rate $rank_allocation_learning_rate --rank_max 50 --rank_average 20 &
+  export WANDB_NOTES="rank_allocation_weights initialized to zero, same scheduling, rank_max 50, average rank 20, discrete rank on the last epoch" # run description
+  python scripts/run_glue_adaptive.py --target_task cola --wandb_disabled False --seed 0 --rank_allocation_learning_rate $rank_allocation_learning_rate --rank_max 50 --rank_average 20 --epoch 51 &
 done
 
 wait
