8954c51a86b46f458d6896ea1c0a8b5bada2838c
mallahova <alismalahova31@gmail.com>
2025-01-24 19:45:36 +0100

    Added initialization options

--- tasks/adaptive_rank_allocation/rank_max_25/rank_5_25_lr_best.sh
+++ tasks/adaptive_rank_allocation/rank_max_25/rank_5_25_lr_best.sh
@@ -10,7 +10,7 @@ SEED=$1
 for rank_allocation_learning_rate in 0.01 0.002 0.02; do
   export WANDB_PROJECT="adaptive_rank_allocation_l_r"
   export CUDA_VISIBLE_DEVICES=0  # Assign GPU 0
-  export WANDB_NOTES="rank_allocation_weights initialized to zero, same scheduling, discrete rank on the last epoch, rank_min is 5" # run description
+  export WANDB_NOTES="rank_allocation_weights initialized to quadratic, same scheduling, discrete rank on the last epoch, rank_min is 5" # run description
   python scripts/run_glue_adaptive.py --target_task cola --wandb_disabled False  --seed $SEED --rank_allocation_learning_rate $rank_allocation_learning_rate --epoch 51 --rank_min 5 --rank_average 20 &
 done
 
