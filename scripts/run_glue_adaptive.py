20d28cb7c83e0ef8c5d2fb2176fdea34ca5b44cd
mallahova <alismalahova31@gmail.com>
2025-03-09 13:12:05 +0100

    Added 90% memory experiment

--- scripts/run_glue_adaptive.py
+++ scripts/run_glue_adaptive.py
@@ -36,8 +36,10 @@ def glue_main(args):
                          --alpha_min {args.alpha_min} \
                          --rank_allocation_learning_rate {args.rank_allocation_learning_rate} \
                          --save_steps 0 \
-                         --rank_average {args.rank_average}"""
+                         --memory_size {args.memory_size} \
+                    """
                     os.system(run_str)
+                    print(run_str)
 
 
 if __name__ == "__main__":
@@ -53,6 +55,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", required=False, default=32)
     parser.add_argument("--epoch", required=False, default=50)
     parser.add_argument("--rank_average", required=False, default=None)
+    parser.add_argument("--memory_size", required=False, default=None)
     args = parser.parse_args()
 
     glue_main(args)
