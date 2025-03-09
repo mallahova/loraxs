68ac06b9b0ddbfd8aee6ffed7f9a860449321085
mallahova <alismalahova31@gmail.com>
2025-03-09 13:23:56 +0100

    Added 90% memory experiment

--- tasks/adaptive_rank_allocation/90_percent_memory_experiment/script_1.sh
+++ tasks/adaptive_rank_allocation/90_percent_memory_experiment/script_1.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=10
 
 
-for seed in 0 1 2; do
+for seed in 3 4; do
   export WANDB_PROJECT="90_percent_memory_experiment"
   export CUDA_VISIBLE_DEVICES=0  # Assign GPU 0
   export WANDB_NOTES="Memory is 10% less than memory for r=25 for every parameter matrix, rank_allocation_weights initialized to randn, same scheduling, discrete rank on the last epoch, rank_min is 5" # run description
