aa2d75287226a5f2a9b4e3b3d6637aaef3c4cbbb
mallahova <alismalahova31@gmail.com>
2025-03-09 21:29:52 +0100

    Added 90% memory experiment

--- tasks/adaptive_rank_allocation/90_percent_memory_experiment/script.sh
+++ tasks/adaptive_rank_allocation/90_percent_memory_experiment/script.sh
@@ -10,7 +10,7 @@ for seed in 0 1 2; do
   export WANDB_PROJECT="90_percent_memory_experiment"
   export CUDA_VISIBLE_DEVICES=0  # Assign GPU 0
   export WANDB_NOTES="Memory is 10% less than memory for r=25 for every parameter matrix, rank_allocation_weights initialized to randn, same scheduling, discrete rank on the last epoch, rank_min is 5" # run description
-  python scripts/run_glue_adaptive.py --target_task cola --wandb_disabled False  --seed $seed --rank_allocation_learning_rate 0.002 --epoch 51 --rank_min 5 --memory_size 54000&
+  python scripts/run_glue_adaptive.py --target_task cola --wandb_disabled False  --seed $seed --rank_allocation_learning_rate 0.01 --epoch 51 --rank_min 5 --memory_size 56400&
 done
 
 wait
