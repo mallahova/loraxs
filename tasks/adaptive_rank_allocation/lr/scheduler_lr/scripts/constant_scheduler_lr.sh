41fcf1497c742064e3d15321c41c187d1f98e17a
mallahova <alismalahova31@gmail.com>
2025-04-15 15:37:40 +0200

    Added constant scheduler experiment

--- tasks/adaptive_rank_allocation/lr/scheduler_lr/scripts/constant_scheduler_lr.sh
+++ tasks/adaptive_rank_allocation/lr/scheduler_lr/scripts/constant_scheduler_lr.sh
@@ -12,8 +12,8 @@ for rank_allocation_lr in 0.002 0.01 0.02; do
   export CUDA_VISIBLE_DEVICES=0  # Assign GPU 0
   export WANDB_NOTES="rank_allocation_weights initialized to random, same scheduling, rank_max  is 30, average rank is 20, rank_min is 5. discrete rank on the last epoch. constant scheduler for lr"
   python scripts/run_glue_adaptive.py --target_task cola --wandb_disabled False  --seed $SEED \
-    --rank_allocation_lr $rank_allocation_lr --epoch 50  --rank_min 5 --rank_max 30 --rank_average 20 --epochs_rank_discrete 1 \
-    --lr_scheduler constant_schedule &
+  --rank_allocation_lr $rank_allocation_lr --epoch 50  --rank_min 5 --rank_max 30 --rank_average 20 --epochs_rank_discrete 1 \
+  --lr_scheduler constant_schedule &
 done
 
 wait
