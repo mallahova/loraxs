cf4a7d82f2f2c262ea900c319b0b17262f1d5fb0
mallahova <alismalahova31@gmail.com>
2025-05-27 13:49:49 +0200

    Added experiments on qnli and sst2

--- scripts/run_glue_adaptive.py
+++ scripts/run_glue_adaptive.py
@@ -7,57 +7,53 @@ def glue_main(args):
     model_name = "roberta-large"
     seeds = [int(args.seed)] if args.seed else [0, 1, 2, 3, 4]
     wandb_disabled = args.wandb_disabled
-    learning_rates=[1e-3]
-    cls_learning_rates=[5e-3]
     if args.rank_max is None:
         if args.rank_start is not None:
             args.rank_max= args.rank_start
         else:
             args.rank_max=args.rank_average
-    for lr in learning_rates:
-        for cls_lr in cls_learning_rates:
-            for seed in seeds:
-                results_dir = f"results_{task}_{args.rank_min}_{args.rank_max}_{args.alpha_min}_{args.alpha_max}_{seed}_{args.rank_allocation_lr}_{args.rank_average}"
-                run_str = [
-                    f'WANDB_DISABLED="{wandb_disabled}"',
-                    "python", "main_glue_adaptive.py",
-                    "--model_name_or_path", model_name,
-                    "--lora_rank", str(args.rank_max),
-                    "--task_name", task,
-                    "--seed", str(seed),
-                    "--per_device_train_batch_size", str(args.batch_size),
-                    "--num_train_epochs", str(args.epoch),
-                    "--output_dir", results_dir,
-                    "--learning_rate", str(lr),
-                    "--cls_learning_rate", str(cls_lr),
-                    "--do_train", "--do_eval",
-                    "--max_seq_length", "128",
-                    "--evaluation_strategy", "epoch",
-                    "--logging_steps", "20",
-                    "--overwrite_output_dir",
-                    "--save_steps", "0",
-                ]
+        for seed in seeds:
+            results_dir = f"results_{task}_{args.rank_min}_{args.rank_max}_{args.alpha_min}_{args.alpha_max}_{seed}_{args.rank_allocation_lr}_{args.rank_average}"
+            run_str = [
+                f'WANDB_DISABLED="{wandb_disabled}"',
+                "python", "main_glue_adaptive.py",
+                "--model_name_or_path", model_name,
+                "--lora_rank", str(args.rank_max),
+                "--task_name", task,
+                "--seed", str(seed),
+                "--per_device_train_batch_size", str(args.batch_size),
+                "--num_train_epochs", str(args.epoch),
+                "--output_dir", results_dir,
+                "--learning_rate", str(args.lr),
+                "--cls_learning_rate", str(args.cls_lr),
+                "--do_train", "--do_eval",
+                "--max_seq_length", "128",
+                "--evaluation_strategy", "epoch",
+                "--logging_steps", "20",
+                "--overwrite_output_dir",
+                "--save_steps", "0",
+            ]
 
-                # Helper function to add arguments conditionally
-                def add_arg(arg_name, arg_value):
-                    if arg_value is not None:
-                        run_str.extend([f"--{arg_name}", str(arg_value)])
+            # Helper function to add arguments conditionally
+            def add_arg(arg_name, arg_value):
+                if arg_value is not None:
+                    run_str.extend([f"--{arg_name}", str(arg_value)])
 
-                add_arg("rank_average", args.rank_average)
-                add_arg("rank_min", args.rank_min)
-                add_arg("rank_max", args.rank_max)
-                add_arg("memory_start", args.memory_start)
-                add_arg("memory_end", args.memory_end)
-                add_arg("epochs_memory_start", args.epochs_memory_start)
-                add_arg("epochs_memory_start_to_end", args.epochs_memory_start_to_end)
-                add_arg("epochs_rank_discrete", args.epochs_rank_discrete)
-                add_arg("alpha_min", args.alpha_min)
-                add_arg("alpha_max", args.alpha_max)
-                add_arg("alpha_scheduler", args.alpha_scheduler)
-                add_arg("rank_allocation_lr", args.rank_allocation_lr)
-                add_arg("lr_scheduler", args.lr_scheduler)
-                os.system(" ".join(run_str))
-                print(run_str)
+            add_arg("rank_average", args.rank_average)
+            add_arg("rank_min", args.rank_min)
+            add_arg("rank_max", args.rank_max)
+            add_arg("memory_start", args.memory_start)
+            add_arg("memory_end", args.memory_end)
+            add_arg("epochs_memory_start", args.epochs_memory_start)
+            add_arg("epochs_memory_start_to_end", args.epochs_memory_start_to_end)
+            add_arg("epochs_rank_discrete", args.epochs_rank_discrete)
+            add_arg("alpha_min", args.alpha_min)
+            add_arg("alpha_max", args.alpha_max)
+            add_arg("alpha_scheduler", args.alpha_scheduler)
+            add_arg("rank_allocation_lr", args.rank_allocation_lr)
+            add_arg("lr_scheduler", args.lr_scheduler)
+            os.system(" ".join(run_str))
+            print(run_str)
 
 
 if __name__ == "__main__":
@@ -67,6 +63,10 @@ if __name__ == "__main__":
     parser.add_argument("--target_task", required=False, default="cola")
     parser.add_argument("--batch_size", required=False, default=32)
     parser.add_argument("--epoch", required=False, default=50)
+
+    parser.add_argument("--lr", required=False, default=1e-3)
+    parser.add_argument("--cls_lr", required=False, default=5e-3)
+
 
     parser.add_argument("--rank_average", required=False, default=None)
 
--- tasks/adaptive_rank_allocation/not_cola/sst2/scripts/best_preset.sh
+++ tasks/adaptive_rank_allocation/not_cola/sst2/scripts/best_preset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=adaptive_loraxs
 #SBATCH --partition=dgx
-#SBATCH --qos=normal
+#SBATCH --qos=quick
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=5
 
@@ -11,6 +11,7 @@ export WANDB_PROJECT="adalora_sst2" #project folder name
 export CUDA_VISIBLE_DEVICES=0  # Assign GPU 0
 export WANDB_NOTES="rank_allocation_weights initialized to random, no alpha scheduler, rank_max is 25, average rank is 20, rank_min is 5. discrete rank on the last epoch. linear_schedule_with_warmup for lr"
 python scripts/run_glue_adaptive.py --target_task sst2 --wandb_disabled False  --seed $SEED \
---rank_allocation_lr $rank_allocation_lr --epoch 50  --rank_min 5 --rank_max 25 --rank_average 20 --epochs_rank_discrete 1 \
+--lr 2e-3 --cls_lr 1e-3 \
+--rank_allocation_lr $rank_allocation_lr --epoch 20  --rank_min 5 --rank_max 25 --rank_average 20 --epochs_rank_discrete 1 \
 --lr_scheduler linear_schedule_with_warmup \
 --alpha_min 0.5 --alpha_max 3
